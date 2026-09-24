a42711abf3baad84f0b7e9c34b991cdb529f9894
LauPuiKa-2627 <laupuika03@gmail.com>
2026-09-24 08:09:00 +0000

    [skip actions] lab/lab04/ch04_t15_big_if.py

--- lab/lab04/ch04_t15_big_if.py
+++ lab/lab04/ch04_t15_big_if.py
@@ -6,7 +6,7 @@ def grade_converter(grade: int) -> str:
         return "B"
     elif grade >= 70 and < 80:
         return "C"
-    elif grade >= 90:
+    elif grade >= 65 and < 70:
         return "D"
     else:
         return "F"
