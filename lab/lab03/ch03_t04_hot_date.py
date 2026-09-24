750a9c9450731a28403ecaaf3790c4c941f1ffdb
LauPuiKa-2627 <laupuika03@gmail.com>
2026-09-24 08:44:39 +0000

    [skip actions] lab/lab03/ch03_t04_hot_date.py

--- lab/lab03/ch03_t04_hot_date.py
+++ lab/lab03/ch03_t04_hot_date.py
@@ -1,4 +1,4 @@
 from datetime import datetime
 
 now = datetime.now()
-print("%02d/%02d/%02d" % (now.hour, now.minute, and now.second))
+print("%02d/%02d/%02d" % (now.month, now.minute, and now.second))
